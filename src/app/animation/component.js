d52c0902e5591e8a87c35bca56829c9333fa3aa9
Sean Bohan <pixelpicosean@gmail.com>
2015-08-10 15:05:03 +0800

    Finish basic style of animation list

--- src/app/animation/component.js
+++ src/app/animation/component.js
@@ -9,10 +9,19 @@ export default {
         m('section.anim-preview', 'Anim Preview')
       ]),
       m('div.anim-editor-lower', [
-        m('ul.anim-list', [
-          m('li.anim-list-item', 'idle'),
-          m('li.anim-list-item', 'run'),
-          m('li.anim-list-item', 'jump')
+        m('div.anim-list', [
+          m('header.anim-list-header', 'Animations'),
+          m('ul.anim-list-container', [
+            m('li.anim-list-item', 'idle'),
+            m('li.anim-list-item', 'run'),
+            m('li.anim-list-item.is-selected', 'jump'),
+            m('li.anim-list-item', 'walk'),
+            m('li.anim-list-item', 'fly'),
+            m('li.anim-list-item', 'dash'),
+            m('li.anim-list-item', 'fall'),
+            m('li.anim-list-item', 'die'),
+            m('li.anim-list-item', 'born')
+          ])
         ]),
         m('ul.anim-settings', [
           m('li.anim-settings-item', 'Speed'),
@@ -23,11 +32,11 @@ export default {
             m('a.anim-sequence-controls-play', 'Play')
           ]),
           m('ul.anim-sequence-editor', [
-            m('li.anim-sequence-editor-frame', '0'),
-            m('li.anim-sequence-editor-frame', '1'),
-            m('li.anim-sequence-editor-frame', '2'),
-            m('li.anim-sequence-editor-frame', '3'),
-            m('li.anim-sequence-editor-frame', '4')
+            m('li.anim-sequence-editor-item', '0'),
+            m('li.anim-sequence-editor-item', '1'),
+            m('li.anim-sequence-editor-item', '2'),
+            m('li.anim-sequence-editor-item', '3'),
+            m('li.anim-sequence-editor-item', '4')
           ])
         ])
       ])
