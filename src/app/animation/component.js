21af31f35aae3d1be597ff59b0e2103ae1fcbb16
Sean Bohan <pixelpicosean@gmail.com>
2015-08-15 19:52:03 +0800

    Use DOM instead of PIXI canvas for animation frame editor.

--- src/app/animation/component.js
+++ src/app/animation/component.js
@@ -1,52 +1,6 @@
 export default {
   controller: function() {
-    let ctrl = {
-      framesRenderer: null,
-
-      updateFrames: function(el, initialized) {
-        if (initialized) {
-          ctrl.renderFrames();
-        }
-        else {
-          ctrl.initFramesView(el);
-        }
-      },
-
-      initFramesView: function(el) {
-        var w = el.offsetWidth;
-        var h = el.offsetHeight;
-        ctrl.framesRenderer = PIXI.autoDetectRenderer(w, h, {
-          view: el,
-          resolution: window.devicePixelRatio
-        });
-
-        ctrl.framesContainer = new PIXI.Container();
-
-        var rect, frame;
-        for (var i = 0; i < 6; i++) {
-          rect = new PIXI.Graphics();
-          rect.beginFill(0xffc107);
-          rect.drawRect(0, 0, 40, 60);
-          rect.endFill();
-
-          rect.position.set(20 + i * 50, 20);
-
-          frame = new PIXI.Text(i + 1 + '', {
-            font: '24px Verdana',
-            fill: 'white'
-          });
-          frame.position.set(20 - frame.width * 0.5, 14);
-          rect.addChild(frame);
-
-          ctrl.framesContainer.addChild(rect);
-        }
-
-        ctrl.renderFrames();
-      },
-      renderFrames: function() {
-        ctrl.framesRenderer.render(ctrl.framesContainer);
-      }
-    };
+    let ctrl = {};
 
     return ctrl;
   },
@@ -82,7 +36,15 @@ export default {
           m('div.anim-sequence-controls', [
             m('a.anim-sequence-controls-play', 'PLAY')
           ]),
-          m('canvas.anim-sequence-editor', { config: ctrl.updateFrames })
+          m('ul.anim-sequence-editor', [
+            m('li.anim-sequence-editor-frame', 'A'),
+            m('li.anim-sequence-editor-frame', 'B'),
+            m('li.anim-sequence-editor-frame', 'C'),
+            m('li.anim-sequence-editor-frame', 'D'),
+            m('li.anim-sequence-editor-frame', 'E'),
+            m('li.anim-sequence-editor-frame', 'F'),
+            m('li.anim-sequence-editor-frame', 'G')
+          ])
         ])
       ])
     ]);
