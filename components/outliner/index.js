69610ef31f0957f9cbf414d535e229cef2476c33
Sean Bohan <pixelpicosean@gmail.com>
2016-02-04 15:32:47 +0800

    Update style of outliner component.

--- components/outliner/index.js
+++ components/outliner/index.js
@@ -20,16 +20,20 @@ export default (container, model$, doAction) => {
   // View
   const EMPTY = [];
 
+  let selectedId = -1;
   const viewItem = (obj) => h(`li`, [
-    h(`label.${css.name}.${css[obj.type]}`, { on: { click: [select, obj.id] } }, obj.name || 'object'),
+    h(`label.${css.name}.${css[obj.type]}${selectedId === obj.id ? '.' + css.selected : ''}`, { on: { click: [select, obj.id] } }, obj.name || 'object'),
     h(`input.${css.toggle}`, { props: { type: 'checkbox' } }),
     h(`ol`, (obj.children || EMPTY).map(viewItem)),
   ]);
 
-  const view = (model) => h('section.outliner', [
-    h('header', 'Outliner'),
-    h(`ol.${css.tree}`, model.children.map(viewItem)),
-  ]);
+  const view = (model) => {
+    selectedId = model.selected;
+    return h(`section.${css.outliner}`, [
+      h('header', 'OUTLINER'),
+      h(`ol.${css.tree}`, model.children.map(viewItem)),
+    ]);
+  }
 
   // Update view based on model changes
   model$.map(view).scan(patch, container)
