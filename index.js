9593fa7a31e5467a4faa79e37d605b7740800b8c
Sean Bohan <pixelpicosean@gmail.com>
2016-02-05 21:43:55 +0800

    Move operators to their own modules.

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ import css from './style.css';
 import context from './context';
 import data from './data';
 
+// Operators
+import ops from './ops';
+import './ops/object';
+
 // Components
 import outliner from './components/outliner';
 import inspector from './components/inspector';
@@ -28,64 +32,6 @@ const init = () => ({
   context: context(),
   data: data(),
 });
-
-let nextObjNameIdx = 0;
-const createContainer = (model, { name, x, y }) => ({
-  id: model.data.nextObjectId(),
-  type: 'Container',
-  name: name || ('object_' + (nextObjNameIdx++)),
-  children: [],
-  x: x || 0, y: y || 0,
-  rotation: 0,
-  scaleX: 1, scaleY: 1,
-  alpha: 1,
-  pivotX: 0, pivotY: 0,
-  skewX: 0, skewY: 0,
-  visible: true,
-});
-
-const createSprite = (model, { name, x, y, texture }) => (Object.assign(createContainer(model, { name, x, y }), {
-  type: 'Sprite',
-  anchorX: 0, anchorY: 0,
-  blendMode: 'NORMAL',
-  texture: texture,
-}));
-
-const factoryMethods = {
-  'Container': createContainer,
-  'Sprite': createSprite,
-};
-
-// Operators
-const ops = {
-  object: {
-    SELECT: (model, param) => {
-      model.context.selected = param;
-
-      return model;
-    },
-
-    ADD: (model, param) => {
-      // Create object instance
-      const obj = factoryMethods[param.type](model, param);
-
-      // Save to object store
-      model.data.objectStore[obj.id] = obj;
-
-      // Add as child of current selected object
-      let parent = model.data.getObjectById(model.context.selected);
-      if (parent) {
-        parent.children.push(obj.id);
-      }
-      // Add to root is no object is selected
-      else {
-        model.data.children.push(obj.id);
-      }
-
-      return model;
-    },
-  },
-};
 
 // Operation dispatcher
 let emitter;
---
+++ ops/object.js
@@ -0,0 +1,57 @@
+import ops from './index';
+
+let nextObjNameIdx = 0;
+const createContainer = (model, { name, x, y }) => ({
+  id: model.data.nextObjectId(),
+  type: 'Container',
+  name: name || ('object_' + (nextObjNameIdx++)),
+  children: [],
+  x: x || 0, y: y || 0,
+  rotation: 0,
+  scaleX: 1, scaleY: 1,
+  alpha: 1,
+  pivotX: 0, pivotY: 0,
+  skewX: 0, skewY: 0,
+  visible: true,
+});
+
+const createSprite = (model, { name, x, y, texture }) => (Object.assign(createContainer(model, { name, x, y }), {
+  type: 'Sprite',
+  anchorX: 0, anchorY: 0,
+  blendMode: 'NORMAL',
+  texture: texture,
+}));
+
+const factoryMethods = {
+  'Container': createContainer,
+  'Sprite': createSprite,
+};
+
+// Add operators to "object" namespace
+ops.object = {
+  SELECT: (model, param) => {
+    model.context.selected = param;
+
+    return model;
+  },
+
+  ADD: (model, param) => {
+    // Create object instance
+    const obj = factoryMethods[param.type](model, param);
+
+    // Save to object store
+    model.data.objectStore[obj.id] = obj;
+
+    // Add as child of current selected object
+    let parent = model.data.getObjectById(model.context.selected);
+    if (parent) {
+      parent.children.push(obj.id);
+    }
+    // Add to root is no object is selected
+    else {
+      model.data.children.push(obj.id);
+    }
+
+    return model;
+  },
+};
